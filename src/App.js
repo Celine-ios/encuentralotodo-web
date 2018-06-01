1e5e2ad34971b3c115b48569c1e2eb5f6f8d318a
u809748758 <u809748758@juanpablo.tk>
2018-06-01 08:40:27 -0430

    Juan here

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/icon.png';
 import './App.css';
 
 class App extends Component {
+      sendIt = () => {
+      alert("Hello");
+    }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <div style={{marginTop: '10%'}}>
+            <img src={logo} alt="Hello"/>
+          </div> 
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <form onSubmit={this.sendIt} method="post" className="login">
+            <div>
+              <label htmlFor="email" style={{color: '#fff'}}>Email</label>
+              <br />
+              <input type="text" name="email" className="input" placeholder="Email"/>
+            </div>
+            <div>
+              <label htmlFor="password" style={{color: '#fff'}}>Contraseña</label>
+              <br />
+              <input type="password" name="password" className="input" placeholder="Contraseña"/>
+            </div>
+            <div>
+              <button type="submit" className="btn btn-purple" style={{width: '60%', height: '3em',margin: '20%'}}>Iniciar Sesión</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
